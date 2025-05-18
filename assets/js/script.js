91b105e199cfea9f20e2c182d9d0a543e465997a
beakbreader <jimmy4374@outlook.com>
2025-05-19 00:27:23 +1000

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-
-function searchFunction() {
-  let input = document.getElementById('search').value.toLowerCase();
-  let mainContent = document.getElementById('main-content');
-  mainContent.innerHTML = '<p>Search results for: ' + input + '</p>';
-}
